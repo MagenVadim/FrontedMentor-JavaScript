55a1e97060647cd4fc7fb06870db71f6d2c9cbc1
MagenVadim <63845671+MagenVadim@users.noreply.github.com>
2023-07-11 18:52:05 +0300

    Add files via upload

---
+++ faq-accordion-card-main/script.js
@@ -0,0 +1,13 @@
+var quest = document.querySelectorAll(".question");
+quest.forEach((question)=>{
+    question.addEventListener("click", () => {
+        if(question.parentNode.classList.contains("active")){
+            question.parentNode.classList.toggle("active")
+        } else{
+            quest.forEach(question=>question.parentNode.classList.remove("active"));
+            question.parentNode.classList.add("active")
+        }
+    })
+
+
+})
